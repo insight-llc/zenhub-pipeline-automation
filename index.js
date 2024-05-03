a5281deb1d3ab483ae06b645637e2f6950f3eaf4
Mike Bronner <mike@genealabs.com>
2024-05-03 16:35:47 -0700

    WIP

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ import("@octokit/graphql")
 
 async function getConfiguredPipeline(workspace) {
     console.log("input", typeof core.getInput("pull-request-state-mapping"));
-    const mapping = core.getInput("pull-request-state-mapping");
-    console.log("pullRequestState", core.getInput("pull-request-state-mapping"), payload.pull_request.state, mapping);
+    const mapping = JSON.parse(core.getInput("pull-request-state-mapping"));
+    console.log("pullRequestState", payload.pull_request.state, mapping, typeof mapping);
     const configuredPipeline = mapping[payload.pull_request.state];
     const pipelines = await getPipelines(workspace.id);
     const pipeline = pipelines
