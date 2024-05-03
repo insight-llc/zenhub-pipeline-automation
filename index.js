0e950412fbf5eabd69a332ca74611d9909cf1b01
Mike Bronner <mike@genealabs.com>
2024-05-03 13:07:47 -0700

    Created initial functionality.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 const { graphql } = require("@octokit/graphql");
 
 const graphqlWithAuth = graphql.defaults({
@@ -8,6 +8,37 @@ const graphqlWithAuth = graphql.defaults({
         authorization: `Bearer ${core.getInput("zenhub-graphql-personal-api-key")}`,
     },
 });
+const payload = github.context.payload;
+
+function getConfiguredPipeline(workspace, pullRequestState) {
+    const configuredPipeline = core.getInput("zenhub-pipeline")[pullRequestState];
+    const pipeline = getPipelines(workspace.id)
+        .find(function (workspacePipeline) {
+            return workspacePipeline.name === configuredPipeline;
+        });
+
+    return pipeline;
+}
+
+async function getPipelines(workspaceId) {
+    const variables = {
+        workspaceId: workspaceId,
+    };
+    const query = `
+        query ($workspaceId: ID!) {
+            workspace(id: $workspaceId) {
+                pipelines {
+                    nodes {
+                        id
+                        name
+                    }
+                }
+            }
+        }
+    `;
+
+    return await graphqlWithAuth(query, variables);
+}
 
 async function getWorkspaces() {
     const variables = {
@@ -33,27 +64,27 @@ async function getWorkspaces() {
         }
     `;
 
-    return await graphqlWithAuth(query, variables);
+    return await graphqlWithAuth(query, variables)
+        .data
+        .viewer
+        .searchWorkspaces
+        .nodes;
 }
 
-async function reviewRequiresChanges(workspaceId) {
+async function moveToPipeline(pipeline) {
     const variables = {
-        workspace: core.getInput("zenhub-workspace"),
+        issueId: payload.issue.id,
+        pipeline: pipeline.id,
+        position: "top",
     };
     const query = `
-        query {
-            viewer {
-                id
-                searchWorkspaces(query: "$workspace") {
-                nodes {
-                        id
+        mutation ($issueId: ID!, $pipelineId: ID!, $position: Int!) {
+            moveIssueToPipelineAndPosition(input: {issueId: $issueId, pipelineId: $pipelineId, position: $position}) {
+                issue {
+                    id
+                    title
+                    pipeline {
                         name
-                    repositoriesConnection {
-                        nodes {
-                                id
-                                name
-                            }
-                        }
                     }
                 }
             }
@@ -64,27 +95,25 @@ async function reviewRequiresChanges(workspaceId) {
 };
 
 try {
-    getWorkspaces()
-        .then((result) => {
-            return reviewRequiresChanges(result.id);
-            console.log(result);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
+    const workspaces = getWorkspaces();
+
+    if (workspaces.length === 0) {
+        core.setFailed(`No workspaces with the name "${core.getInput("zenhub-workspace")}" found.`);
+    }
+
+    for (const workspace of workspaces) {
+        const pullRequestState = (payload.review || {}).state || "";
+        const pipeline = getConfiguredPipeline(workspace, pullRequestState);
+
+        if (! pipeline) {
+            continue;
+        }
+
+        moveToPipeline(workspace, pipeline);
+    }
+
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
-}
-
-const getWorkspaces = function () {
-
-    return 'workspaceId';
 }
