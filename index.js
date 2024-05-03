941c20464b646e4ca8b17ecd8458420703b856a4
Mike Bronner <mike@genealabs.com>
2024-05-03 14:34:56 -0700

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let graphql;
 const core = require("@actions/core");
 const github = require("@actions/github");
-import { graphql } from "@octokit/graphql";
 
 import("@octokit/graphql")
     .then((octokit) => {
